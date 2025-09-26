49840294b3c97d55378d7f58ffb63211f6335b5c
amit helios tech <amit.heliostech@gmail.com>
2025-09-26 17:29:35 +0530

    added codes

--- task-management-application-backend/index.js
+++ task-management-application-backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 const server = createServer(app);
 export const io = new Server(server, {
   cors: {
-    origin: "https://task-management-application-opal.vercel.app/",
+    origin: "https://task-management-application-opal.vercel.app",
     credentials: true,
   },
 });
@@ -41,7 +41,7 @@ io.on("connection", (socket) => {
 
 app.use(
   cors({
-    origin: "https://task-management-application-opal.vercel.app/",
+    origin: "https://task-management-application-opal.vercel.app",
     credentials: true,
   })
 );
