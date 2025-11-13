3acbb6ada0bc8bc38d55a8a690cd7ee4d7d9ee7a
amit helios tech <amit.heliostech@gmail.com>
2025-11-13 21:41:14 +0530

    new commit

--- task-management-application-frontend/vite.config.ts
+++ task-management-application-frontend/vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import checker from 'vite-plugin-checker'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , checker({ typescript: false }),  tailwindcss(),],
-  
-})
+  plugins: [react(), checker({ typescript: false }), tailwindcss()],
+  preview: {
+    allowedHosts: ["promanage.duckdns.org"],
+    host: "0.0.0.0",
+    port: 4173,
+  },
+});
